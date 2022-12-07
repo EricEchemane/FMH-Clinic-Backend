11250cb27f963e0898b6fe91ac84f43387e76f1b
Eric Echemane <eechemane29@gmail.com>
2022-12-07 11:22:10 +0800

    setup environment configurations

---
+++ src/config/configuration.ts
@@ -0,0 +1,30 @@
+export default () => {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      return {
+        PGDATABASE: 'fmh_clinic',
+        PGHOST: 'localhost',
+        PGPASSWORD: '19126222',
+        PGPORT: 5432,
+        PGUSER: 'postgres',
+      };
+    case 'test':
+      return {
+        PGDATABASE: 'test',
+        PGHOST: 'localhost',
+        PGPASSWORD: '19126222',
+        PGPORT: 5432,
+        PGUSER: 'postgres',
+      };
+    case 'production':
+      return {
+        PGDATABASE: 'railway',
+        PGHOST: 'containers-us-west-125.railway.app',
+        PGPASSWORD: '0sD1FaSiIYmL1DUdgunX',
+        PGPORT: 7177,
+        PGUSER: 'postgres',
+      };
+    default:
+      return null;
+  }
+};
