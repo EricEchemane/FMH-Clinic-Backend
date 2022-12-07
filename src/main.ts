c22e5ef5ffc42c61fdb5b9d0a99a64b907902ca9
Eric Echemane <eechemane29@gmail.com>
2022-12-07 11:37:55 +0800

    cors enabled

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
   const PORT = process.env.PORT ? parseInt(process.env.PORT) : 2900;
   await app.listen(PORT);
 }
