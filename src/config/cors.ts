7a3202244fe413514f2df5b0cf6c6414e2292566
Eric Echemane <eechemane29@gmail.com>
2022-12-07 11:45:29 +0800

    configured cors whitelist

---
+++ src/config/cors.ts
@@ -0,0 +1,5 @@
+export const whiteList = [
+  'http://localhost:3000',
+  'https://fmh-clinic.vercel.app',
+  'https://fmh-clinic.ml',
+];
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { whiteList } from './config/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: whiteList,
+  });
   const PORT = process.env.PORT ? parseInt(process.env.PORT) : 2900;
   await app.listen(PORT);
 }
