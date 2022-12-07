7b1715822e1ac523eccf860f553718ee9138778b
Eric Echemane <eechemane29@gmail.com>
2022-12-07 12:00:14 +0800

    remove test code

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor(private configService: ConfigService) {}
   getHello(): string {
-    // return 'hello, world!';
-    return this.configService.get('PGDATABASE');
+    return 'hello, world!';
   }
 }
