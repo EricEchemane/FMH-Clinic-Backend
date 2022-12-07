08721c8131fce67f4e078417e6a0ecad664f6509
Eric Echemane <eechemane29@gmail.com>
2022-12-07 11:59:06 +0800

    rename configuration to environment inside /config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import configuration from './config/configuration';
+import environment from './config/environment';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [configuration],
+      load: [environment],
     }),
   ],
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return 'hello, world!';
+    // return 'hello, world!';
+    return this.configService.get('PGDATABASE');
   }
 }
