bad80601945f637cad355940ce05ebba0e75015a
Eric Echemane <eechemane29@gmail.com>
2022-12-07 11:14:28 +0800

    dynamic port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT ? parseInt(process.env.PORT) : 2900;
+  await app.listen(PORT);
 }
 bootstrap();
