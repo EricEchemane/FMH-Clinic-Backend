e018f8cec4ede47dd793070f5970f14f4e645a13
Eric Echemane <eechemane29@gmail.com>
2022-12-09 13:05:57 +0800

    swagger ui integration

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { whiteList } from './config/cors';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig } from './config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: whiteList,
   });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   const PORT = process.env.PORT ? parseInt(process.env.PORT) : 2900;
   await app.listen(PORT);
 }
