a93cfa5796a80b02eb1f2fe6e03031d639159359
Eric Echemane <eechemane29@gmail.com>
2022-12-09 11:40:39 +0800

    feature/purchase-module: PATCH /purchaes/:id

--- src/purchase/dto/update-purchase.dto.ts
+++ src/purchase/dto/update-purchase.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreatePurchaseDto } from './create-purchase.dto';
+import { IsBoolean } from 'class-validator';
+
+class CreatePurchaseDto {
+  @IsBoolean()
+  archived: boolean;
+}
 
 export class UpdatePurchaseDto extends PartialType(CreatePurchaseDto) {}
