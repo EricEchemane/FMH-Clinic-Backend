dc1bc74271e4bee208528cfe8864f45518066390
Eric Echemane <eechemane29@gmail.com>
2022-12-09 10:01:33 +0800

    product entity and create and update dto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,33 @@
-export class CreateProductDto {}
+import {
+  IsBoolean,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  @Min(1)
+  unit_price: number;
+
+  @IsInt()
+  @Min(0)
+  stock: number;
+
+  @IsString()
+  @IsNotEmpty()
+  image_url: string;
+
+  @IsBoolean()
+  archived: boolean;
+}
--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,25 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column({ type: 'float' })
+  unit_price: number;
+
+  @Column({ type: 'int' })
+  stock: number;
+
+  @Column()
+  image_url: string;
+
+  @Column({ default: false })
+  archived: boolean;
+}
