7bd50049a06179c1cff193b2c8bf777b38f34cf5
Eric Echemane <eechemane29@gmail.com>
2022-12-13 12:37:50 +0800

    added whitelist

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,6 +1,4 @@
 export const whiteList = [
   'http://localhost:3000',
-  'https://fmh-clinic.vercel.app',
-  'https://fmh-clinic.ml',
-  'https://fmh-animal-clinic.vercel.app/',
+  'https://fmh-animal-clinic.vercel.app',
 ];
