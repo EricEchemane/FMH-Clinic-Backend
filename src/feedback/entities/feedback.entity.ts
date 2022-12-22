fa6a7d2a697888f7879463e75a196f60cdeaddbc
Eric Echemane <eechemane29@gmail.com>
2022-12-23 07:56:26 +0800

    fixed Feedback entity - missing primary column and added to the module for Feature

--- src/feedback/entities/feedback.entity.ts
+++ src/feedback/entities/feedback.entity.ts
@@ -1,8 +1,17 @@
 import { User } from 'src/user/entities';
-import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Feedback {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
   @Column({ type: 'int' })
   rating: number;
 
--- src/feedback/feedback.module.ts
+++ src/feedback/feedback.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FeedbackService } from './feedback.service';
 import { FeedbackController } from './feedback.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Feedback } from './entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Feedback])],
   controllers: [FeedbackController],
   providers: [FeedbackService],
 })
