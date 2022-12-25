b3ebaffb6da16d037f23510e662443eb02f84fec
Eric Echemane <eechemane29@gmail.com>
2022-12-25 22:29:40 +0800

    new server

--- src/common/config/cors.ts
+++ src/common/config/cors.ts
@@ -1,6 +1,6 @@
 export const getOrigin = () => {
   if (process.env.NODE_ENV === 'production') {
-    return 'https://fmh.onrender.com';
+    return 'https://fmh.fly.dev';
   }
   return 'http://localhost:3000';
 };
