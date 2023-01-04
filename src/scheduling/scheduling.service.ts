e74c186f1755ad9d125074a9a618f726428c302d
Eric Echemane <eechemane29@gmail.com>
2023-01-04 09:34:11 +0800

    remove sechduling prevention of toady scheduling

--- src/scheduling/scheduling.service.ts
+++ src/scheduling/scheduling.service.ts
@@ -23,15 +23,15 @@ export class SchedulingService {
   ) {}
 
   async create(createScheduleDto: CreateScheduleDto, user: RequestUser) {
-    const hasScheduleToday = await this.schedulesRepository.findOneBy({
-      date: new Date(createScheduleDto.date),
-      status: ScheduleStatus.pending,
-    });
-    if (hasScheduleToday) {
-      throw new BadRequestException(
-        'You already have a pending schedule today',
-      );
-    }
+    //const hasScheduleToday = await this.schedulesRepository.findOneBy({
+    //  date: new Date(createScheduleDto.date),
+    //  status: ScheduleStatus.pending,
+    //});
+    //if (hasScheduleToday) {
+    //  throw new BadRequestException(
+    //    'You already have a pending schedule today',
+    //  );
+    //}
 
     let newEntry = this.schedulesRepository.create(createScheduleDto);
     newEntry = await this.schedulesRepository.save({
